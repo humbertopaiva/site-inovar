60ea358595b1d18a3c58a1de111eec9411f8c1df
Humberto Paiva <humbertomoreira93@gmail.com>
2025-04-15 19:35:27 -0300

    build: add static export to next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
     domains: ["img.freepik.com", "static.wixstatic.com"],
     remotePatterns: [
