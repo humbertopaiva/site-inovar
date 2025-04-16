77f830a0988da1811fd59eb59233185a5bde16f1
Humberto Paiva <humbertomoreira93@gmail.com>
2025-04-16 10:31:16 -0300

    feat(hero): remove vertical padding from img

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -210,7 +210,7 @@ const Hero = () => {
                 src="/hero-inovar.jpg"
                 alt="Inovar Assessoria Empresarial"
                 fill
-                className="object-cover max-h-[70vh] rounded-sm md:p-0 p-4"
+                className="object-cover max-h-[70vh] rounded-sm md:p-0 px-4"
                 priority
               />
             </motion.div>
